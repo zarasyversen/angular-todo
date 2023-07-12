e7b55d35f97849cb6c72edffb741eab469b7f141
Zara Syversen <zara.syversen@ninetech.com>
2023-07-12 12:11:21 +0200

    Add Todo Search

--- src/app/todos/todo-list/todo-list.component.ts
+++ src/app/todos/todo-list/todo-list.component.ts
@@ -1,6 +1,13 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { TodoItem } from 'src/app/types/TodoItem';
-import { faEdit, faTrash, faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-todo-list',
@@ -13,9 +20,19 @@ export class TodoListComponent {
   @Output() onTodoDeleted = new EventEmitter<number>();
   @Output() onTodoEdit = new EventEmitter<{ id: number; newTitle: string }>();
 
-  faEdit = faEdit;
-  faTrash = faTrash;
-  faCheck = faCheck;
+  //@ViewChild('searchInput') searchInput: ElementRef;
+
+  faTimes = faTimes;
+  faSearch = faSearch;
+
+  filteredTodos: TodoItem[] = [];
+  isSearching = false;
+  searchTerm = '';
+  hasNoResults = false;
+
+  ngOnInit() {
+    this.filteredTodos = this.todos;
+  }
 
   deleteTodo(todoId: number) {
     this.onTodoDeleted.emit(todoId);
@@ -26,5 +43,33 @@ export class TodoListComponent {
       id: id,
       newTitle: title,
     });
+  }
+
+  toggleSearch() {
+    this.isSearching = !this.isSearching;
+    this.searchTerm = '';
+    this.filteredTodos = this.todos;
+    this.hasNoResults = false;
+  }
+
+  searchTodos() {
+    if (this.searchTerm) {
+      const matchedTodos = this.todos.filter((item) => {
+        return (
+          item.title.toLowerCase().indexOf(this.searchTerm.toLowerCase()) !== -1
+        );
+      });
+
+      if (matchedTodos.length > 0) {
+        this.filteredTodos = matchedTodos;
+        this.hasNoResults = false;
+      } else {
+        this.hasNoResults = true;
+        this.filteredTodos = [];
+      }
+    } else {
+      this.filteredTodos = this.todos;
+      this.hasNoResults = false;
+    }
   }
 }
